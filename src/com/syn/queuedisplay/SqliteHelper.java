4f4919ed0b613d007773784c941566abef9e06d6
Synature-Jitthapong <jitthapong@synaturegroup.com>
2013-07-24 23:16:34 +0700

    add SqliteHelper class

---
+++ src/com/syn/queuedisplay/SqliteHelper.java
@@ -0,0 +1,31 @@
+package com.syn.queuedisplay;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqliteHelper extends SQLiteOpenHelper {
+	private static String dbName = "queue";
+
+	private static final String createTbConfig = "CREATE TABLE config (" +
+			" server_ip INTEGER, " + 
+			" service_name TEXT, " +
+			" video_path TEXT, " +
+			" logo_path TEXT );";
+
+	public SqliteHelper(Context context) {
+		super(context, dbName, null, 1);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(createTbConfig);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS config");
+		onCreate(db);
+	}
+
+}
