f5cf173ad12e51a79eb623ec32a7e82b64270124
Synature-Jitthapong <jitthapong@synaturegroup.com>
2013-07-25 18:16:10 +0700

    add QueueData class

---
+++ src/com/syn/queuedisplay/QueueData.java
@@ -0,0 +1,33 @@
+package com.syn.queuedisplay;
+
+public class QueueData {
+	private String serverIp;
+	private String serviceName;
+	private String videoPath;
+	private String logoPath;
+	
+	public String getServerIp() {
+		return serverIp;
+	}
+	public void setServerIp(String serverIp) {
+		this.serverIp = serverIp;
+	}
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	public String getVideoPath() {
+		return videoPath;
+	}
+	public void setVideoPath(String videoPath) {
+		this.videoPath = videoPath;
+	}
+	public String getLogoPath() {
+		return logoPath;
+	}
+	public void setLogoPath(String logoPath) {
+		this.logoPath = logoPath;
+	}
+}
